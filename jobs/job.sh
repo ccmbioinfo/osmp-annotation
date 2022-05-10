c00a609979c2515b359876c90cb39d34b0685f0e
Hannah Le <hannah.lgbhan@gmail.com>
2022-05-10 14:49:48 -0400

    added job script

---
+++ jobs/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+export TOKEN=$(curl --location --request POST 'https://keycloak.ccmdev.ca/auth/realms/ssmpdev/protocol/openid-connect/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'password=secret' --data-urlencode 'username=ssmp-user' --data-urlencode 'client_id=ssmp-server' --data-urlencode 'realm=ssmpdev' --data-urlencode 'grant_type=password' | jq -r '.access_token')
+
+module load nextflow Singularity;
+
+if [[ -z $1 ]];
+then 
+    echo "No JSON passed."
+else
+    JSON=$1
+    variants=$(nextflow run annotation.nf -profile annotation --json $JSON)
+    echo $variants | jq length
+
+    TOKEN=$(curl --location --request POST 'https://keycloak.ccmdev.ca/auth/realms/ssmpdev/protocol/openid-connect/token' --header 'Content-Type: application/x-www-form-urlencoded' --data-urlencode 'password=secret' --data-urlencode 'username=ssmp-user' --data-urlencode 'client_id=ssmp-server' --data-urlencode 'realm=ssmpdev' --data-urlencode 'grant_type=password' | jq -r '.access_token')
+    curl --request POST \
+        -H 'Content-Type: application/json' \
+        --header "Authorization: Bearer $TOKEN" \
+        --data "{'operationName':'OnSlurmResponse','variables':{ 'jobId': $SLURM_JOB_ID, 'variants': $variants}}" \
+        https://osmp.ccmdev.ca/graphql
+fi
+
